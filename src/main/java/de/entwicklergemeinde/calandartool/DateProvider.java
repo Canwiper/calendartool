eeb45279bcb025a58bcf9216a3ba7798ce68f125
Canwiper <tim@kannewischer.de>
2017-01-22 16:58:53 +0100

    first commit

---
+++ src/main/java/de/entwicklergemeinde/calandartool/DateProvider.java
@@ -0,0 +1,27 @@
+package de.entwicklergemeinde.calandartool;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class DateProvider {
+
+	private List<DateAppointment> dateAppointments = new ArrayList<DateAppointment>();
+
+	public void addDate(DateAppointment dateAppointment) {
+		dateAppointments.add(dateAppointment);
+	}
+
+	public List<DateAppointment> getDatesForToday() {
+		List<DateAppointment> result = new ArrayList<DateAppointment>();
+		Calendar cal = Calendar.getInstance();
+		for (DateAppointment dateAppointment : dateAppointments) {
+			Calendar calAppointment = Calendar.getInstance();
+			calAppointment.setTime(dateAppointment.getAppointmentDate());
+			if (cal.equals(calAppointment))
+				result.add(dateAppointment);
+		}
+		return result;
+	}
+
+}
---
+++ src/test/java/de/entwicklergemeinde/calandartool/DatesProviderTest.java
@@ -0,0 +1,35 @@
+package de.entwicklergemeinde.calandartool;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatesProviderTest {
+
+	private DateProvider testDateProvider = new DateProvider();
+	
+	@Before
+	public void setUp() {
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(2017, 1, 12);
+
+		DateAppointment dateAppointment1 = new DateAppointment(new Date(), "Test appointment");
+		DateAppointment dateAppointment2 = new DateAppointment(new Date(), "Test appointment 2");
+		DateAppointment dateAppointment3 = new DateAppointment(calendar.getTime(), "Test appointment 3");
+
+		testDateProvider.addDate(dateAppointment1);
+		testDateProvider.addDate(dateAppointment2);
+		testDateProvider.addDate(dateAppointment3);
+	}
+
+	@Test
+	public void testGetDatesForToday() {
+		
+		assertEquals(2, testDateProvider.getDatesForToday().size());
+	}
+
+}
